14e6f0b9cbee8151cb170ceee66950ef1b05e444
Andrew Plotkin <erkyrath@eblong.com>
2012-04-08 13:09:00 -0400

    Need the terp-specific window subclasses.

---
+++ TerpSrc/TerpGlkWindows.h
@@ -0,0 +1,13 @@
+/* TerpGlkWindows.h: Interpreter-specific window buffer subclasses, for the IosGlk library
+ for IosGlulxe, an IosGlk port of the Glulxe interpreter.
+ Designed by Andrew Plotkin <erkyrath@eblong.com>
+ http://eblong.com/zarf/glk/
+ */
+
+#import <UIKit/UIKit.h>
+#import "TerpGlkWindows.h"
+#import "GlkWinBufferView.h"
+
+@interface TerpGlkWinBufferView : GlkWinBufferView
+
+@end
---
+++ TerpSrc/TerpGlkWindows.m
@@ -0,0 +1,32 @@
+/* TerpGlkWindows.m: Interpreter-specific window buffer subclasses, for the IosGlk library
+ for IosGlulxe, an IosGlk port of the Glulxe interpreter.
+ Designed by Andrew Plotkin <erkyrath@eblong.com>
+ http://eblong.com/zarf/glk/
+ */
+
+#import "TerpGlkWindows.h"
+#import "StyledTextView.h"
+#import "TerpGlkViewController.h"
+#import "TerpGlkDelegate.h"
+
+@implementation TerpGlkWinBufferView
+
+- (id) initWithWindow:(GlkWindowState *)winref frame:(CGRect)box {
+	self = [super initWithWindow:winref frame:box];
+	if (self) {
+		TerpGlkViewController *glkviewc = [TerpGlkViewController singleton];
+		int val = glkviewc.terpDelegate.colorscheme;
+		self.textview.indicatorStyle = (val==2 ? UIScrollViewIndicatorStyleWhite : UIScrollViewIndicatorStyleDefault);
+	}
+	return self;
+}
+
+- (void) uncacheLayoutAndStyles {
+	[super uncacheLayoutAndStyles];
+
+	TerpGlkViewController *glkviewc = [TerpGlkViewController singleton];
+	int val = glkviewc.terpDelegate.colorscheme;
+	self.textview.indicatorStyle = (val==2 ? UIScrollViewIndicatorStyleWhite : UIScrollViewIndicatorStyleDefault);
+}
+
+@end
